9230df9297eac976a821271051d1ca3772e16184
Anjana Kurup <akurup94@gmail.com>
2016-09-15 18:24:30 +0000

    prefinal21

--- prefinal/script.js
+++ prefinal/script.js
@@ -1,10 +1,15 @@
 //alert("Testing");
 var btn=document.getElementById("btn");
+var btn2=document.getElementById("btn2");
 var ccUrl="https://www.codecademy.com/akurup94";
+
+
 btn.onmousedown = showAcademy;
+btn.onmousedown = newTabAcademy;
+
 
 function showAcademy(){
-    alert("going to: "+ ccUrl);
+    //alert("going to: "+ ccUrl);
     document.location=ccUrl;
 }
 function newTabAcademy(){
