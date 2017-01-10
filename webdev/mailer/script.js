8833ad7ae542796630704b58c1f41574e91d892e
Anjana Kurup <akurup94@gmail.com>
2017-01-10 20:03:53 +0000

    working email

---
+++ webdev/mailer/script.js
@@ -0,0 +1,13 @@
+/*
+    An attempt to mail a contact that is selected
+*/
+document.getElementById("chooser").onchange = sendEmail;
+// Here is the sendEmail function
+function sendEmail(eventObject){
+    var source = eventObject.target;
+    var index = source.selectedIndex;
+    var options = source.options
+    var email = options[index].value;
+    window.location = "mailto:"+email;
+    
+}
